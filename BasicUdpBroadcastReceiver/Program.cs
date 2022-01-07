992dabec415178f61c99a76c5b4b0a837cc2095f
Emil Wilhelm Winther <emil377g@edu.easj.dk>
2022-01-07 12:15:04 +0100

    Add project files.

---
+++ BasicUdpBroadcastReceiver/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace BasicUdpBroadcastReceiver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UdpClient client = new UdpClient(7000);
+            byte[] data;
+
+            while (true)
+            {
+                IPEndPoint remoteEP = null;
+
+                data = client.Receive(ref remoteEP);
+
+                string str = Encoding.UTF8.GetString(data);
+                Console.WriteLine("Received message from user: " + str);
+            }
+        }
+    }
+}
